6c3efbe2fa5aa18df069662e46351564cdd69570
Spacedu <contato@spacedu.com.br>
2021-10-12 10:55:07 -0300

    Pegar Usuários com Dapper

--- Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/UsuarioRepository.cs
+++ Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace eCommerce.API.Repositories
 {
@@ -16,15 +17,10 @@ namespace eCommerce.API.Repositories
             _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
-        private static List<Usuario> _db = new List<Usuario>()
-        {
-            new Usuario(){ Id=1, Nome="Filipe Rodrigues", Email="filipe.rodrigues@gmail.com" },
-            new Usuario(){ Id=2, Nome="Marcelo Rodrigues", Email="marcelo.rodrigues@gmail.com"},
-            new Usuario(){ Id=3, Nome="Jessica Rodrigues", Email="jessica.rodrigues@gmail.com"}
-        };
+        //ADO.NET > Dapper: Micro-ORM (MER <-> POO)
         public List<Usuario> Get()
         {
-            return _db;
+            return _connection.Query<Usuario>("SELECT * FROM Usuarios").ToList();
         }
 
         public Usuario Get(int id)
@@ -58,5 +54,12 @@ namespace eCommerce.API.Repositories
         {
             _db.Remove(_db.FirstOrDefault(a => a.Id == id));
         }
+
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario(){ Id=1, Nome="Filipe Rodrigues", Email="filipe.rodrigues@gmail.com" },
+            new Usuario(){ Id=2, Nome="Marcelo Rodrigues", Email="marcelo.rodrigues@gmail.com"},
+            new Usuario(){ Id=3, Nome="Jessica Rodrigues", Email="jessica.rodrigues@gmail.com"}
+        };
     }
 }
