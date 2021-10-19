aeb605e3de685c504e333435075ef8d968701f1e
Spacedu <contato@spacedu.com.br>
2021-10-19 16:52:16 -0300

    Dapper Contrib > Estruturando o projeto.

---
+++ Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Controllers/ContribUsuariosController.cs
@@ -0,0 +1,61 @@
+﻿using eCommerce.API.Models;
+using eCommerce.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/Contrib/Usuarios")]
+    [ApiController]
+    public class ContribUsuariosController : ControllerBase
+    {
+        private IUsuarioRepository _repository;
+        public ContribUsuariosController()
+        {
+            _repository = new ContribUsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.Get());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _repository.Get(id);
+
+            if (usuario == null)
+            {
+                return NotFound(); //ERRO HTTP: 404 - Not Found
+            }
+
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody] Usuario usuario)
+        {
+            _repository.Insert(usuario);
+            return Ok(usuario);
+        }
+        [HttpPut]
+        public IActionResult Update([FromBody] Usuario usuario)
+        {
+            _repository.Update(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Codigo-Fonte/Seção 04/eCommerce.API/eCommerce.API/Repositories/ContribUsuarioRepository.cs
@@ -0,0 +1,35 @@
+﻿using eCommerce.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.API.Repositories
+{
+    public class ContribUsuarioRepository : IUsuarioRepository
+    {
+        public List<Usuario> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
